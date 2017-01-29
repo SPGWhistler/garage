e331e4dcb0fafde8deedbd29fc896b0611127492
Tony Petty <tpetty@synacor.com>
2017-01-29 14:06:26 -0500

    testing

--- listener.php
+++ listener.php
@@ -22,7 +22,8 @@ $mqtt->close();
 function procmsg($topic, $msg){
 	echo "$msg\n";
 	if ($msg === "get") {
-		echo shell_exec("cat /sys/class/gpio/gpio1017/value");
+		$value = shell_exec("cat /sys/class/gpio/gpio1017/value");
+		$mqtt->publish("/garage", $value, 0);
 	} else {
 		shell_exec("sudo sh -c 'echo 0 > /sys/class/gpio/gpio1021/value'");
 		shell_exec("sudo sh -c 'echo 1 > /sys/class/gpio/gpio1021/value'");
