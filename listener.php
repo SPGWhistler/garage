3f293351400efd1645fe467d3e3086886d641024
Tony Petty <tpetty@synacor.com>
2017-01-29 14:37:40 -0500

    testing

--- listener.php
+++ listener.php
@@ -1,6 +1,13 @@
 <?php
 require("phpMQTT.php");
 
+#Setup gpio
+shell_exec("sudo sh -c 'echo 1017 > /sys/class/gpio/export'");
+shell_exec("sudo sh -c 'echo 1022 > /sys/class/gpio/export'");
+shell_exec("sudo sh -c 'echo out > /sys/class/gpio/gpio1017/direction'");
+shell_exec("sudo sh -c 'echo out > /sys/class/gpio/gpio1022/direction'");
+shell_exec("sudo sh -c 'echo 0 > /sys/class/gpio/gpio1022/value'");
+
 $mqtt = new phpMQTT("192.168.1.214", 1883, "garageDoor");
 
 if(!$mqtt->connect()){
@@ -21,13 +28,13 @@ $mqtt->close();
 
 function procmsg($topic, $msg){
 	global $mqtt;
-	echo "$msg\n";
+	echo $msg . "\n";
 	if ($msg === "get") {
 		$value = shell_exec("cat /sys/class/gpio/gpio1017/value");
 		$mqtt->publish("/garage", $value, 0);
 	} else {
-		shell_exec("sudo sh -c 'echo 0 > /sys/class/gpio/gpio1022/value'");
 		shell_exec("sudo sh -c 'echo 1 > /sys/class/gpio/gpio1022/value'");
+		shell_exec("sudo sh -c 'echo 0 > /sys/class/gpio/gpio1022/value'");
 	}
 }
 ?>
