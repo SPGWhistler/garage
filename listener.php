6607017d506fdfac81f1fd32006a4555e3eae009
Tony Petty <tpetty@synacor.com>
2017-01-29 20:26:47 -0500

    testing

--- listener.php
+++ listener.php
@@ -2,11 +2,14 @@
 require("phpMQTT.php");
 
 #Setup gpio
-for ($i = 16; $i <= 23; $i++) {
-	shell_exec("sudo sh -c 'echo 10$i > /sys/class/gpio/export'");
-	shell_exec("sudo sh -c 'echo out > /sys/class/gpio/gpio10$i/direction'");
-	shell_exec("sudo sh -c 'echo 1 > /sys/class/gpio/gpio10$i/value'");
-}
+$door = 1016;
+$closedSensor = 1023;
+shell_exec("sudo sh -c 'echo $door > /sys/class/gpio/export'");
+shell_exec("sudo sh -c 'echo out > /sys/class/gpio/gpio$door/direction'");
+shell_exec("sudo sh -c 'echo 1 > /sys/class/gpio/gpio$door/value'");
+shell_exec("sudo sh -c 'echo $closedSensor > /sys/class/gpio/export'");
+shell_exec("sudo sh -c 'echo out > /sys/class/gpio/gpio$closedSensor/direction'");
+shell_exec("sudo sh -c 'echo 1 > /sys/class/gpio/gpio$closedSensor/value'");
 
 $mqtt = new phpMQTT("192.168.1.214", 1883, "garageDoor");
 
@@ -30,19 +33,11 @@ function procmsg($topic, $msg){
 	global $mqtt;
 	echo $msg . "\n";
 	if ($msg === "get") {
-		for ($i = 16; $i <= 23; $i++) {
-			$value = shell_exec("cat /sys/class/gpio/gpio10$i/value");
-			$mqtt->publish("/garage/door/status", $value, 0);
-			echo $i ."\n";
-			sleep(3);
-		}
+		$value = shell_exec("cat /sys/class/gpio/gpio$closedSensor/value");
+		$mqtt->publish("/garage/door/status", $value, 0);
 	} else {
-		for ($i = 16; $i <= 23; $i++) {
-			shell_exec("sudo sh -c 'echo 1 > /sys/class/gpio/gpio10$i/value'");
-			shell_exec("sudo sh -c 'echo 0 > /sys/class/gpio/gpio10$i/value'");
-			echo $i ."\n";
-			sleep(3);
-		}
+		shell_exec("sudo sh -c 'echo 0 > /sys/class/gpio/gpio$door/value'");
+		shell_exec("sudo sh -c 'echo 1 > /sys/class/gpio/gpio$door/value'");
 	}
 }
 ?>
